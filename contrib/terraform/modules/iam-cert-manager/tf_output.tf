b71c2828c277284d1eda6009706c8ce7ed33678a
Sören Schneider <56670304+soerenschneider@users.noreply.github.com>
2024-09-22 14:54:41 +0200

    refactor statement

--- contrib/terraform/modules/iam-cert-manager/tf_output.tf
+++ contrib/terraform/modules/iam-cert-manager/tf_output.tf
@@ -1,9 +1,9 @@
 output "access_key_id" {
-  value = "${aws_iam_access_key.user.id}"
+  value = aws_iam_access_key.user.id
 }
 
 output "access_key_secret" {
-  value = "${aws_iam_access_key.user.secret}"
+  value = aws_iam_access_key.user.secret
 }
 
 output "access_key_user" {
