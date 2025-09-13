3431db8fe8b157ff0297659df63d494935573958
Sören Schneider <56670304+soerenschneider@users.noreply.github.com>
2025-09-13 18:22:47 +0200

    improve healthcheck by not requesting full list of files

--- apps/jellyfin/components/storage-healthcheck/healthcheck.sh
+++ apps/jellyfin/components/storage-healthcheck/healthcheck.sh
@@ -2,7 +2,7 @@
 
 curl -sf http://localhost:8096/health || exit 1
 
-if [ -z "$(ls -A "/media")" ]; then
+if ! find /media -mindepth 1 -maxdepth 1 -print -quit | grep -q .; then
   echo "Directory is empty. Exiting with failure."
   exit 1
 fi
