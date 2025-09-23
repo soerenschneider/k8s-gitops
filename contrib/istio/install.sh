dfa6db77c9b94e34fbfd247e8a1d93427c9310b9
Sören Schneider <56670304+soerenschneider@users.noreply.github.com>
2025-09-23 21:18:43 +0200

    improve install script

--- contrib/istio/install.sh
+++ contrib/istio/install.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 
+SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+
 cluster=$1
-operator=istio-operator-$cluster.yaml
+operator="${SCRIPT_DIR}/istio-operator-${cluster}.yaml"
 if ! [[ -f $operator ]]; then
-    echo "no such file: $operator"
-    exit 1
+    operator="${SCRIPT_DIR}/${cluster}"
+    if ! [[ -f $operator ]]; then
+      echo "no such file: $operator"
+      exit 1
+    fi
 fi
 
 istioctl install -f "${operator}"
