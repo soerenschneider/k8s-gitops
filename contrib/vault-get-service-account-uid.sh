02ee6505312c4b1fca457fce5da2352799d49074
Sören Schneider <56670304+soerenschneider@users.noreply.github.com>
2025-09-23 21:17:29 +0200

    improve output

--- contrib/vault-get-service-account-uid.sh
+++ contrib/vault-get-service-account-uid.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
+echo "Service Account UID vault-auth"
 kubectl get serviceaccount -n vault-auth vault-kubernetes-auth -o jsonpath="{.metadata.uid}"
+echo
+
+echo "Service Account UID external-secrets"
+kubectl get serviceaccount -n external-secrets-system external-secrets -o jsonpath="{.metadata.uid}"
 echo
