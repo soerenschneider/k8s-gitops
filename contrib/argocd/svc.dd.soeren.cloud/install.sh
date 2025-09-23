589a0973e8a2211e6375a769b5d7fc7358d68dbd
Sören Schneider <56670304+soerenschneider@users.noreply.github.com>
2025-09-23 21:19:42 +0200

    improve script

--- contrib/argocd/svc.dd.soeren.cloud/install.sh
+++ contrib/argocd/svc.dd.soeren.cloud/install.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env sh
 
+SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+
+kubectl create namespace argocd
 kubectl create secret generic argocd-secret \
     --from-literal=server.secretkey=$(openssl rand -base64 32) \
     --namespace=argocd
+
+kubectl apply -k "${SCRIPT_DIR}"
+kubectl apply -k "${SCRIPT_DIR}"
