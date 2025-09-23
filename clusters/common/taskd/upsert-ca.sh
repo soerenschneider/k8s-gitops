f4312dadc9d2fb7c756d958b3f209a1d97d40e92
Sören Schneider <56670304+soerenschneider@users.noreply.github.com>
2025-09-23 21:08:24 +0200

    remove old clusters

--- clusters/common/taskd/upsert-ca.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-CONFIGMAP_NAME="taskd-ca"
-CONFIGMAP_KEY="ca.crt"
-CONFIGMAP_FILE="configmap-ca.yaml"
-DEFAULT_PKI_URL="pki/im_task"
-
-curl -s "${VAULT_ADDR}/v1/${DEFAULT_PKI_URL}/ca/pem" | \
-  kubectl create configmap "${CONFIGMAP_NAME}" --from-file="${CONFIGMAP_KEY}"=/dev/stdin --dry-run=client -o yaml > "${CONFIGMAP_FILE}"
