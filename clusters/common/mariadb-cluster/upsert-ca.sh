df5c2e7d4dd140216b2f167786aa1fcea318005f
Sören Schneider <56670304+soerenschneider@users.noreply.github.com>
2025-10-26 16:06:33 +0100

    delete unused resources

--- clusters/common/mariadb-cluster/upsert-ca.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-CONFIGMAP_NAME="mariadb-ca"
-CONFIGMAP_KEY="ca.crt"
-CONFIGMAP_FILE="configmap-ca.yaml"
-DEFAULT_PKI_URL="pki/im_srn"
-
-curl -s "${VAULT_ADDR}/v1/${DEFAULT_PKI_URL}/ca_chain" | \
-  kubectl create configmap "${CONFIGMAP_NAME}" --from-file="${CONFIGMAP_KEY}"=/dev/stdin --dry-run=client -o yaml > "${CONFIGMAP_FILE}"
